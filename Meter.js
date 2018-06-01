fd0b43f9b56890ca52ca6df23b2498487b4012e8
@pauloddr <git@pauloddr.com>
2018-06-01 18:45:07 +0000

    add Meter class and docs

---
+++ Meter.js
@@ -0,0 +1,35 @@
+const NOOP = function () {}
+
+class Meter {
+  constructor (min, max, current) {
+    this.minimum = min || 0
+    this.maximum = max || 1
+    this.current = current || 1
+    this.onDepletion = NOOP
+    this.onCompletion = NOOP
+  }
+
+  change (amount) {
+    if (!amount) { // zero == false
+      return
+    }
+    var previousAmount = this.current
+    this.current += amount
+    if (this.current < this.minimum) {
+      this.current = this.minimum
+    }
+    if (this.current > this.maximum) {
+      this.current = this.maximum
+    }
+    if (previousAmount === this.current) {
+      return // no change
+    }
+    if (this.current === this.minimum) {
+      this.onDepletion()
+    } else if (this.current === this.maximum) {
+      this.onCompletion()
+    }
+  }
+}
+
+module.exports = Meter
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# fightron-core
-Fightron Core Objects
+# @fightron/core
+
+__Fightron Engine__ Core Objects that can be used by both Client and Server components.
+
+## Meter
+
+Represents one of the Character's Meters (Health, Special, Shield, Focus, or Revenge).
+
+Properties:
+
+* `minimum` - the minimum amount of meter.
+* `maximum` - the maximum amount of meter.
+* `current` - the current amount of meter.
+* `onDepletion` - a callback for when the meter fully depletes.
+* `onCompletion` - a callback for when the meter becomes full.
+
+Methods:
+
+* `change(amount)` - change the meter amount. Pass negative values to decrease the amount. Automatically calls callbacks if minimum/maximum amounts are reached.
