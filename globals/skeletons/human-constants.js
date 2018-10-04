a47635251633a2a1ae3f99ddf186fe3688f47145
@pauloddr <git@pauloddr.com>
2018-10-04 18:55:19 +0000

    move human size constants to own file
    Will be re-used later for global human-box schematics

---
+++ globals/skeletons/human-constants.js
@@ -0,0 +1,39 @@
+// Measurements in cm.
+// Directions reference, from character POV:
+// - left +x, right -x
+// - front +z, back -z
+
+export const NECK_HEIGHT = 10
+export const SHOULDER_HEIGHT = 3
+export const SHOULDER_OFFSET = 14
+export const CHEST_HEIGHT = 16
+export const CHEST_WIDTH = 32
+export const ABDOMEN_HEIGHT = 14
+export const HIP_HEIGHT = 11
+export const HIP_WIDTH = 31
+export const HIP_DEPTH = 12
+
+export const ARM_UPPER_HEIGHT = 30
+export const ARM_LOWER_HEIGHT = ARM_UPPER_HEIGHT
+
+export const PALM_HEIGHT = 10
+export const FINGER_SEGMENT_HEIGHT = 5
+export const FINGER_SEGMENT_HEIGHT_90 = 4
+export const FINGER_SEGMENT_HEIGHT_80 = 3
+export const FINGER_WIDTH = 3
+
+export const THUMB_WIDTH = FINGER_WIDTH
+export const THUMB_OFFSET = 4.5
+export const THUMB_SEGMENT_HEIGHT = FINGER_SEGMENT_HEIGHT
+
+export const INDEX_OFFSET = 4.5
+export const MIDDLE_OFFSET = 1.4
+export const RING_OFFSET = -1.8
+export const PINKY_OFFSET = -4.9
+
+export const LEG_UPPER_HEIGHT = 50
+export const LEG_LOWER_HEIGHT = LEG_UPPER_HEIGHT
+export const LEG_OFFSET = 8
+
+export const FOOT_HEIGHT = 10
+export const FOOT_BASE_DEPTH = 12
